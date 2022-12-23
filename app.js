a52064652b13374dda947122d1587ce01cfe3c06
adomasr <adomas.razmus@gmail.com>
2022-12-23 21:27:34 +0000

    some functionality and css

--- app.js
+++ app.js
@@ -7,14 +7,26 @@
 //sugestion: for(let element of data.colors) {const color = element.hex.value}
 
 const getColorBtn = document.getElementById('get-color-btn');
+let colorArray = [];
 
 
-getColorBtn.addEventListener('click', getColor);
+getColorBtn.addEventListener('click', renderColors);
 
 
 
 function getColor() {
     fetch('https://www.thecolorapi.com/scheme?hex=ff0000&mode=analogic')
         .then((response) => response.json())
-        .then((data) => console.log(data.colors[0].hex.value));
+        .then((data) => {
+            for (let element of data.colors) {
+                colorArray.push(element.hex.value);
+            }
+        });
+}
+
+
+
+function renderColors() {
+    getColor();
+    console.log(colorArray);
 }
